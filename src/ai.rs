1ea7569575cadd59d48f666b9c1617b1e24761a3
bashar <bashar29@hotmail.fr>
2023-04-25 21:35:18 +0200

    refactoring

--- src/ai.rs
+++ src/ai.rs
@@ -6,14 +6,7 @@ use rand::Rng;
 
 pub fn random_ai(board: &Board, player: &Player) -> Result<Board> {
     debug!("Random AI move generation");
-    let mut legal_moves = Vec::new();
-    for (y, line) in board.iter().enumerate() {
-        for (x, c) in line.iter().enumerate() {
-            if c.is_none() {
-                legal_moves.push((y, x));
-            }
-        }
-    }
+    let legal_moves = find_all_legal_moves(board);
     let new_board = match select_one_random_move(&legal_moves, board, player) {
         Some(b) => b,
         None => return Err(anyhow!("no legal move available")),
@@ -22,22 +15,14 @@ pub fn random_ai(board: &Board, player: &Player) -> Result<Board> {
 }
 
 pub fn finds_winning_moves_ai(board: &Board, player: &Player) -> Result<Board> {
-    debug!("Search a winning move");
-    let mut legal_moves = Vec::new();
-    for (y, line) in board.iter().enumerate() {
-        for (x, c) in line.iter().enumerate() {
-            if c.is_none() {
-                legal_moves.push((y, x));
-            }
-        }
-    }
+    debug!("Search an immediate winning move");
+    let legal_moves = find_all_legal_moves(board);
     for m in &legal_moves {
         let new_board = board::make_move(board, *m, player).unwrap();
         if board::is_move_win(&new_board).is_some() {
             return Ok(new_board)
         }
     }
-
     let new_board = match select_one_random_move(&legal_moves, board, player) {
         Some(b) => b,
         None => return Err(anyhow!("no legal move available")),
@@ -54,6 +39,18 @@ fn select_one_random_move(legal_moves: &Vec<(usize,usize)>, board: &Board, playe
     let chosen_move = legal_moves[rng.gen_range(0..legal_moves.len())];
     let new_board = board::make_move(board, chosen_move, player).unwrap();
     Some(new_board)
+}
+
+fn find_all_legal_moves(board: &Board) -> Vec<(usize,usize)> {
+    let mut legal_moves = Vec::new();
+    for (y, line) in board.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if c.is_none() {
+                legal_moves.push((y, x));
+            }
+        }
+    }
+    legal_moves
 }
 
 #[cfg(test)]
